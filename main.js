79a9a421cdcd4bd1ed98394b50732f5a4c02aee6
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-09-19 08:50:21 -0300

    hg

---
+++ main.js
@@ -0,0 +1,6 @@
+function criarCartao (categoria, pergunta, resposta){
+let container = doument.getElementyByld ('container');
+let cartao = document.createElement('article');
+cartao.className= "carte"
+
+}
