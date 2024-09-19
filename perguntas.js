2bfcca312240943986565c372df03dc443862d80
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-09-19 07:46:01 -0300

    gfd

---
+++ perguntas.js
@@ -0,0 +1,96 @@
+criarCartao ( 
+    'Matemática', 
+    'Qual quadrante sempre será negativo, tanto seno, quanto cosseno?', 
+    'O 3° quadrante') 
+    
+    criarCartao ( 
+    'Matemática', 
+    'O que é um número primo?', 
+    'Um numero que não pode ser formado pela multiplicação de outros números') 
+    
+    criarCartao ( 
+    'Fisica', 
+    'Qual é a formula da energia cinética?', 
+    'Ec= mv2/2') 
+    
+    criarCartao( 
+    'História', 
+    'Em que ano começou a primeira guerra mundial?', 
+    '1914') 
+    
+    criarCartao( 
+    'História', 
+    'Quem foi o primeiro presidente do brasil?', 
+    'Marechal Deodoro da fonseca') 
+    
+    criarCartao ( 
+    'História', 
+    'Quando ocorreu o golpe militar brasileiro?', 
+    '1964') 
+    
+    criarCartao ( 'Geografia', 
+    'Qual a capital da Nicarágua?', 
+    'Managuá') 
+    
+    criarCartao ( 
+    'Geografia', 
+    'De que pais é essa bandeira?', 'Coreia do norte') 
+    
+    criarCartao( 'Geografia', 
+    'Qual é o menor e maior pais do mundo respectivamente?', 
+    'Vaticano e Rússia') 
+    
+    criarCartao( 
+    'Geografia', 
+    'Qual é o relevo predominante no brasil?',
+    'Planaltos') 
+    
+    criarCartao( 
+    'Biologia', 
+    'Quais são as fases da meiose?', 
+    'Prófase, metástase, anáfase e telófase') 
+    
+    criarCartao( 
+    'Biologia', 
+    'Como a malária é transmitida?', 
+    'Pela picada da fêmea do mosquito Anopheles') 
+    
+    criarCartao( 
+    'História', 
+    'O que foi a política do café com leite ?',
+    'Politica que permitiu a burguesia cafeeira de controlar a política monetária e câmbial') 
+    
+    criarCartao( 
+    'Literatura', 
+    'Qual o autor do livro dom casmurro?',
+    'Machado de assis') 
+    
+    criarCartao( 
+    'Fisica', 
+    'O que é o efeito fotoelétrico?', 
+    'É um fenômeno quantico que consiste na emissão de elétrons por algum metal que é Iluminado por radiação eletromagnética') 
+    
+    criarCartao( 
+    'Geografia', 
+    'O que é chuva ácida?', 
+    'É uma consequência da poluição da atmosfera') 
+    
+    criarCartao( 
+    'Artes',
+    'Quem é o autor da obra noite estrelada?', 
+    'Van Gogh') 
+    
+    criarCartao( 
+    'Química', 
+    'Quem disse a frase "Na naturezanada se perde, nada se cria tudo se transforma?', 
+    'Antoine Lavoiser') 
+    
+    criarCartao( 
+    'Quimica', 
+    'Radiação com alto poder de penetração?', 
+    'Gama') 
+    
+    criarCartao( 
+     'Educação fisica', 
+     'Onde surgiu os jogos olímpicos?', 
+     'Atenas, Grécia') 
