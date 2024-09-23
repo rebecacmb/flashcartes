23a639ffc5a7ee3fb5f2ebe5bbd10e839993f50d
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-09-23 07:32:29 -0300

    wt

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 function criarCartao (categoria, pergunta, resposta){
 let container = doument.getElementyByld ('container');
 let cartao = document.createElement('article');
-cartao.className= "carte"
+cartao.className= "cartao"
+cartao.innerHTML =
+<div class="conteudo-cartao">
+<h3>${cartegoria}</h3>
+<div class="pergunta-cartão">
+<p>${pergunta}</p>
+</div>
+<div class="resposta-cartao"> 
+<p>${resposta}</p>
+</div>
+</div>
+
+container.appendChild(cartao);
 
 }
