4cd2c7b07a3bead1ea021a3dce977c41c087255e
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-09-23 08:02:48 -0300

    fd

--- perguntas.js
+++ perguntas.js
@@ -34,7 +34,7 @@ criarCartao (
     
     criarCartao ( 
     'Geografia', 
-    'De que pais é essa bandeira Google (Noto Color Emoji - Unicode 15.1)?', 'Coreia do norte') 
+    'Qual a capital da Nova Zelandia ?', 'Wellington') 
     
     criarCartao( 'Geografia', 
     'Qual é o menor e maior pais do mundo respectivamente?', 
